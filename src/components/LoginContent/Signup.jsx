cf2b0b84b94392411b3551d37e5124e47be951a5
Devesh Swarnkar <deveshswarnkar007@gmail.com>
2023-07-04 16:27:55 +0530

    css change

--- src/components/LoginContent/Signup.jsx
+++ src/components/LoginContent/Signup.jsx
@@ -7,7 +7,7 @@ const Signup = () => {
       <div
         className={` fixed top-0 left-0 right-0 bottom-0 blur-effect-theme w-full h-screen opacity-100 z-[250] grid items-center justify-items-center `}
       >
-        <div className="absolute bg-theme w-[25rem] h-[70%]  sm:w-[20rem] xsm:w-[15rem] rounded-lg shadow shadow-slate-600">
+        <div className="absolute bg-theme w-[25rem] h-[70%]  sm:w-[20rem] xsm:w-[17rem] rounded-lg shadow shadow-slate-600">
           <div className="">
             <button
               type="button"
@@ -18,8 +18,8 @@ const Signup = () => {
               </Link>
             </button>
           </div>
-          <div className="flex items-center justify-around p-1 mt-5">
-            <h1 className="font-bold text-[2rem] ">Signup</h1>
+          <div className="flex items-center justify-around p-1 mt-5 xsm:mb-2">
+            <h1 className="font-bold text-[2rem] xsm:text-[1.5rem]">Signup</h1>
           </div>
 
           <form
@@ -28,38 +28,38 @@ const Signup = () => {
           >
             <label
               htmlFor="username"
-              className="block m-4 uppercase font-bold text-slate-200 " 
+              className="block m-4 uppercase font-bold text-slate-200 xsm:my-2 xsm:text-xs" 
             >
               UserName
             </label>
             <input
               type="text"
               id="username"
-              className="ml-4 w-[90%] px-5 py-1 text-gray-700 bg-gray-200 rounded"
+              className="ml-4 w-[90%] xsm:w-[85%] px-5 py-1 text-gray-700 bg-gray-200 rounded xsm:text-xs"
               placeholder="UserName"
             />
             <label
               htmlFor="email"
-              className="block m-4 uppercase font-bold text-slate-200"
+              className="block m-4 uppercase font-bold text-slate-200 xsm:my-2 xsm:text-xs"
             >
               email
             </label>
             <input
               type="text"
               id="email"
-              className="ml-4 w-[90%] px-5 py-1 text-gray-700 bg-gray-200 rounded"
+              className="ml-4 w-[90%] px-5 py-1 text-gray-700 bg-gray-200 rounded xsm:w-[85%] xsm:text-xs"
               placeholder="Email"
             />
             <label
               htmlFor="password"
-              className="block m-4 uppercase font-bold text-slate-200"
+              className="block m-4 uppercase font-bold text-slate-200 xsm:my-2 xsm:text-xs"
             >
               password
             </label>
             <input
               type="text"
               id="password"
-              className="ml-4 w-[90%] px-5 py-1 text-gray-700 bg-gray-200 rounded"
+              className="ml-4 w-[90%] px-5 py-1 text-gray-700 bg-gray-200 rounded xsm:w-[85%] xsm:text-xs"
               placeholder="Password"
             />
 
@@ -68,7 +68,7 @@ const Signup = () => {
               className=" bg-blue-400 px-2.5 py-1 rounded mx-4 mt-7 border border-blue-800 shadow shadow-slate-800"
             >
               <Link to="/signup">
-                <h1 className=" font-semibold text-slate-200 ">Signup</h1>
+                <h1 className=" font-semibold text-slate-200 xsm:text-xs">Signup</h1>
               </Link>
             </button>
 
